6576ad6fc8b1aa8d784629fabedb3183cca10365
Darshan660 <85669807+Darshan660@users.noreply.github.com>
2023-06-12 18:40:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -271,9 +271,9 @@ if compare_price_checked:
             result_placeholder.write()  # Update the placeholder element with the result
             
             # Add a expander
-            expand_sidebar = st.sidebar.checkbox("Disclamer")
+            expand_sidebar = st.checkbox("Disclamer")
             if expand_sidebar:
-                st.sidebar.caption("The laptop price predictions provided by this application are based on historical data and machine learning algorithms. Actual prices may vary, and we recommend conducting additional research and consulting with experts before making any purchasing decisions.")  
+                st.caption("The laptop price predictions provided by this application are based on historical data and machine learning algorithms. Actual prices may vary, and we recommend conducting additional research and consulting with experts before making any purchasing decisions.")  
                 
         except Exception as e:
             st.error(f"An error occurred during scraping, Please ensure that the specifications are accurately selected. Avoid inputting any arbitrary values that may affect the results.")
